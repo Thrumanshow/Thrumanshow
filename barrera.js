0ff92d637a2814477672b45fd59f58956c003d0f
@cristhiam-quinonez Especialista en Marketing Digital | Estrategias de Crecimiento y SEO <chrisquionez354@gmail.com>
2025-08-27 19:59:43 -0600

    Actualizar barrera.js

--- barrera.js
+++ barrera.js
@@ -1,4 +1,5 @@
-Ejemplo: 
-      { if; make -=>decromatiza
-               AUTENTICIDAD=VALIDACIÓN 
-                           `di_lema: Error d-e-m-o-c-r-@-t-i-c-o
+if (make) {
+    action => "decromatiza";
+    AUTENTICIDAD = VALIDACIÓN;
+    di_lema = "Error d-e-m-o-c-r-@-t-i-c-o";
+}
