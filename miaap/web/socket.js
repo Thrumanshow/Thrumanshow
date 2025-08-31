4510dea38f47eea7931622c9e431fd3cce5a9078
@cristhiam-quinonez Especialista en Marketing Digital | Estrategias de Crecimiento y SEO <chrisquionez354@gmail.com>
2025-08-30 20:09:18 -0600

    Update and rename miaap/socket.io to miaap/web/socket.js

---
+++ miaap/web/socket.js
@@ -0,0 +1,22 @@
+// web/socket.js
+const io = require("socket.io")(3001, {
+  cors: { origin: "*" } // si es desde otro dominio
+});
+
+io.of("/my-16ultrapro").on("connection", (socket) => {
+  console.log(`Nuevo socket conectado: ${socket.id}`);
+
+  socket
+    .timeout(5000)
+    .to("room1")
+    .to(["room2", "room3"])
+    .except("thrumanshow")
+    .emit("hello", (err, responses) => {
+      if (err) console.error("Error enviando mensaje:", err);
+      else console.log("Respuestas recibidas:", responses);
+    });
+
+  socket.on("disconnect", () => {
+    console.log(`Socket desconectado: ${socket.id}`);
+  });
+});
